d4e17807de436659a0960cff5f2260a7cf3dbe07
Jitgitbit <rowowolf@gmail.com>
2020-05-16 13:22:26 +0200

    Impure sort Pipe

--- src/app/reverse.pipe.ts
+++ src/app/reverse.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReversePipe implements PipeTransform {
 
   transform(value: any): any {
-    return value.split('').reverse().join('');
+    return value.split('').reverse().join('');               //-----> rarely usefull
   }
 
 }
--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'sort'
+  name: 'sort',
+  pure: false                       //--------> again this will cause the pipe to recalculate on each time there's a change (WATCH OUT for performance loss!)             
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, propName: string): any {
+    return value.sort((a,b) => {
+      if(a[propName] > b[propName]) {                      //----> this will sort alphabetically !
+        return 1;
+      }else{
+        return -1;
+      }
+    });
   }
 
 }
